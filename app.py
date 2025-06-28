33df82ecd84b3344923d0403af0ab624845000fb
Upreak <connnectionsupreak@gmail.com>
2025-06-28 16:54:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from modules.chat_handler import router as telegram_router
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Resume Chatbot API is running!"}
+    return {"message": "Hello from FastAPI"}
+
+# Include the Telegram webhook router
+app.include_router(telegram_router)
