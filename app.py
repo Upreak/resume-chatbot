7c60887c9ddba648816f8395d09ec8275955c7a0
Upreak <connnectionsupreak@gmail.com>
2025-06-29 23:15:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from modules.chat_handler import router as telegram_router
+from modules.center_module.center_module import router as center_router
 
 app = FastAPI()
 
 # Include the Telegram webhook router
 app.include_router(telegram_router)
+app.include_router(center_router)
