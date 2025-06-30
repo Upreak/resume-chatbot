a6dee54040c51e2bb373f71e75f01a3fff14c18c
Upreak <connnectionsupreak@gmail.com>
2025-06-30 22:52:27 +0530

    Update telegram_api.py

--- shared/telegram_api.py
+++ shared/telegram_api.py
@@ -2,10 +2,25 @@ import os
 import httpx
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+BASE_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}"
 
 async def send_message(chat_id, text):
     async with httpx.AsyncClient() as client:
         await client.post(
-            f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
+            f"{BASE_URL}/sendMessage",
             json={"chat_id": chat_id, "text": text}
+        )
+
+async def send_inline_buttons(chat_id, text, buttons):
+    async with httpx.AsyncClient() as client:
+        await client.post(
+            f"{BASE_URL}/sendMessage",
+            json={
+                "chat_id": chat_id,
+                "text": text,
+                "reply_markup": {
+                    "inline_keyboard": [buttons]
+                },
+                "parse_mode": "HTML"
+            }
         )
